96d09723260c022f1ab2d86c735bde1ceceafb55
Andi <andisurdu23@gmail.com>
2023-09-08 00:27:37 +0300

    Updates

--- assets/index-becae0fb.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./ParticleImg-171b6bb9.js";const t="/JS-Particle-Effects/Christmas-tree.png";new e({parent:document.querySelector(".particle-img.effect1"),canvas_width:450,canvas_height:620,img_src:t},{particleSize:4});
